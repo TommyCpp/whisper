7b9d5804a6c9df36c81a1c8a60bbbe8d2395e62b
Zhongyang Wu <a444529216@hotmail.com>
2018-09-20 23:22:33 -0400

    middleware

--- config.go
+++ config.go
@@ -7,17 +7,31 @@ import (
 
 type Configuration struct {
 	Port           int `json:"port"`
-	MiddlewareSize int `json:"middleware.size"` //todo:test
+	MiddlewareSize int `json:"middlewareSize"`
 }
 
-func ReadConfig(filename string, configuration Configuration) error {
+func NewConfiguration() *Configuration {
+	return &Configuration{
+		Port:           8086,
+		MiddlewareSize: 2,
+	}
+}
+
+func ReadConfig(filename string, configuration *Configuration) error {
 	file, err := os.Open(filename)
 	if err != nil {
 		return err
 	}
 	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&configuration)
+	err = decoder.Decode(configuration)
 	if err != nil {
 		return err
 	}
+	return nil
 }
+
+//func main() {
+//	configuration := NewConfiguration()
+//	ReadConfig("./config.json", configuration)
+//	fmt.Print(configuration.MiddlewareSize)
+//}
---
+++ middleware/encryptionMiddleware.go
@@ -0,0 +1,12 @@
+package middleware
+
+type EncryptionMiddleware struct {
+}
+
+func (encryption *EncryptionMiddleware) afterRead(msg string) {
+
+}
+
+func (encryption *EncryptionMiddleware) beforeWrite(msg string) {
+
+}
